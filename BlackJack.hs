0a53213df04c87ef05b2b93a99fa4cb0720437a4
chrhenr <chrhenr@chalmers.se>
2024-11-20 10:04:28 +0000

    get rid of reduce aces

--- BlackJack.hs
+++ BlackJack.hs
@@ -6,6 +6,7 @@ import System.Random
 
 -- A0
 hand2 = Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty)
+handAce = Add (Card Ace Hearts) (Add (Card Ace Spades) Empty)
 sizeSteps :: [Integer]
 sizeSteps = [ size hand2
             , size (Add (Card (Numeric 2) Hearts)
@@ -28,13 +29,11 @@ display (Add card rest) = displayCard card ++ "\n" ++ display rest
 -- A2
 -- The maximum point value of a hand without going over 21, if possible
 value :: Hand -> Integer
-value hand = reduceAces (initialValue hand) (numberOfAces hand)
-
--- Takes as input: a score and the number of aces that count as 11 in that score.
--- Returns: the maximum score without going over 21, if possible, by converting some aces from 11 to 1.
-reduceAces :: Integer -> Integer -> Integer
-reduceAces score numAces = if score > 21 && numAces > 0 then
-                                reduceAces (score - 10) (numAces - 1) else score
+value hand = if score > 21 && numAces > 0 then
+                score - 10*numAces else score
+            where
+                score = initialValue hand
+                numAces = numberOfAces hand
 
 -- The total point value of a hand, counting all aces as 11
 initialValue :: Hand -> Integer
@@ -62,52 +61,8 @@ gameOver hand = value hand > 21
 -- Returns the winner, bank or guest, of the hand.
 -- In order to win, the player must have a score higher than the bank but not higher than 21
 winner :: Hand -> Hand -> Player
-winner guestHand bankHand = if not (gameOver guestHand) && (guestValue > bankValue || gameOver bankHand) then Guest else Bank
-    where guestValue = value guestHand 
-          bankValue  = value bankHand
-
---B1
-
-addToBottom :: Card -> Hand -> Hand
-addToBottom card Empty = Add card Empty
-addToBottom card (Add top rest) = Add top (addToBottom card rest)
-
-(<+) :: Hand -> Hand -> Hand
-hand <+ Empty = hand
-hand <+ (Add card rest) = addToBottom card hand <+ rest
-
-prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool 
-prop_onTopOf_assoc p1 p2 p3 = p1<+(p2<+p3) == (p1<+p2)<+p3
-
-prop_size_onTopOf :: Hand -> Hand -> Bool
-prop_size_onTopOf h1 h2 = size (h1 <+ h2) == size h1 + size h2
-
---B2
-numerics :: [Rank]
-numerics = [Numeric i | i <- [2..10]]
-
-ranks :: [Rank]
-ranks = numerics ++ [Jack, Queen, King, Ace]
-
-suits :: [Suit]
-suits = [Hearts, Spades, Diamonds, Clubs]
-
-allCards :: [Card]
-allCards = [Card rank suit | suit <- suits, rank <- ranks]
-
-fullDeck :: Hand
-fullDeck = foldr addToBottom Empty allCards
-
---B3
-draw :: Hand -> Hand -> (Hand,Hand)
-draw Empty _ = error "draw: The deck is empty."
-draw (Add card rest) hand = (rest, Add card hand)
-
---B4
-playBank :: Hand -> Hand
-playBank deck = playBankHelper deck Empty
-
-playBankHelper :: Hand -> Hand -> Hand
-playBankHelper deck hand = if value biggerHand >= 16 then biggerHand
-    else playBankHelper smallerDeck biggerHand
-    where (smallerDeck,biggerHand) = draw deck hand
+winner guestHand bankHand 
+        | not (gameOver guestHand) && (guestValue > bankValue || gameOver bankHand) = Guest
+        | otherwise = Bank  
+        where guestValue = value guestHand 
+              bankValue  = value bankHand
